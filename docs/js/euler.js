eb0f18aaa14495e4c3863a862aacdbe083a6cb4e
Nick S <nicholas.stapleton97@gmail.com>
2019-05-06 23:12:33 -0700

    added basic css

---
+++ docs/js/euler.js
@@ -0,0 +1,12 @@
+var reset = setInterval(function() {
+	console.log("reset animation!");
+	var imgs = document.getElementsByClassName("animated");
+	for(let img of imgs) {
+		// This is not working as intended?
+		// img.classList.remove("animated");
+		// img.classList.add("animated");
+		img.style.animation = "none";
+		img.offsetHeight;
+		img.style.animation = null;
+	}
+}, 30000);
