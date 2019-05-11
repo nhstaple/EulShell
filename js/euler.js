d7e7c372d38625cb3a68b8571e0615c6c203a7f2
Nick S <nicholas.stapleton97@gmail.com>
2019-05-10 20:02:47 -0700

    added read more/less

--- js/euler.js
+++ js/euler.js
@@ -1,3 +1,26 @@
+
+var readMore = false;
+
+function toggleReadMore() {
+	if(readMore == true) {
+		readMore = false;
+	} else {
+		readMore = true;
+	}
+
+	if(readMore) {
+		document.getElementById("readMoreWrapper").style.display = "flex";
+		document.getElementById("readMore").style.display = "none";
+		document.getElementById("readLess").style.display = "block";
+		// document.getElementById("readMoreWrapper").scrollIntoView();
+	} else {
+		document.getElementById("readMoreWrapper").style.display = "none";
+		document.getElementById("readMore").style.display = "block";
+		document.getElementById("readLess").style.display = "none";
+		document.getElementById("changelog").scrollIntoView();
+	}
+}
+
 var reset = setInterval(function() {
 	console.log("reset animation!");
 	var imgs = document.getElementsByClassName("animated");
